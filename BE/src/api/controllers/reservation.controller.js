497c73cad6a06f707de2436ef2195aaa3bf70d35
danggiabao2005 <baongu2005@gmail.com>
2026-06-16 21:43:46 +0700

    add api cancel + getAll reservation

--- BE/src/api/controllers/reservation.controller.js
+++ BE/src/api/controllers/reservation.controller.js
@@ -48,6 +48,45 @@ class ReservationController {
             next(error);
         }
     }
+
+    getAllReservations = async (req, res, next) => {
+        try {
+            const { page, limit, status } = req.query;
+            const result = await this.#reservationService.getAllReservations({
+                page: parseInt(page) || 1,
+                limit: parseInt(limit) || 10,
+                status,
+            });
+
+            res.status(200).json({
+                status: 'success',
+                data: {
+                    reservations: result.reservations,
+                    pagination: result.pagination,
+                },
+                message: 'Reservations fetched successfully',
+            });
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    cancelReservation = async (req, res, next) => {
+        try {
+            const { reservationId } = req.params;
+            const { userId } = req.user;
+            const cancelledReservation = await this.#reservationService.cancelReservation({ driverId: userId, reservationId });
+            res.status(200).json({
+                status: 'success',
+                data: {
+                    cancelledReservation,
+                    message: 'Reservation cancelled successfully',
+                },
+            });
+        } catch (error) {
+            next(error);
+        }
+    }
 }
 
 export default ReservationController;
